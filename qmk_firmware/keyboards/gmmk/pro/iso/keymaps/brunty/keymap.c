16d37da597ed07e184efcccb665b3bda613b6573
Matt Brunt <Brunty@users.noreply.github.com>
2022-02-20 17:07:29 +0000

    Add side RGB

--- qmk_firmware/keyboards/gmmk/pro/iso/keymaps/brunty/keymap.c
+++ qmk_firmware/keyboards/gmmk/pro/iso/keymaps/brunty/keymap.c
@@ -58,6 +58,63 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 };
+
+
+
+// RGB LED layout ISO
+
+// led number, function of the key
+
+//  68, Side led 01    0, ESC      6, F1       12, F2       18, F3       23, F4       28, F5       34, F6       39, F7       44, F8       50, F9       56, F10      61, F11      66, F12      70, Prt                  Rotary(Mute)   69, Side led 12
+//  71, Side led 02    1, `~       7, 1        13, 2        19, 3        24, 4        29, 5        35, 6        40, 7        45, 8        51, 9        57, 0        62, -_       79, (=+)     86, BackSpc              73, Home       72, Side led 13
+//  74, Side led 03    2, Tab      8, Q        14, W        20. E        25, R        30, T        36, Y        41, U        46, I        52, O        58, P        63, [{       90, ]}                                76, PgUp       75, Side led 14
+//  77, Side led 04    3, Caps     9, A        15, S        21, D        26, F        31, G        37, H        42, J        47, K        53, L        59, ;:       64, '"       95, #        97, Enter                87, PgDn       78, Side led 15
+//  81, Side led 05    4, Sh_L     67,\        10, Z        16, X        22, C        27, V        32, B        38, N        43, M        48, ,<       54, .<       60, /?                    91, Sh_R     94, Up      83, End        82, Side led 16
+//  84, Side led 06    5, Ct_L     11,Win_L    17, Alt_L                                           33, SPACE                              49, Alt_R    55, FN       65, Prt                   96, Left     98, Down    80, Right      85, Side led 17
+//  88, Side led 07                                                                                                                                                                                                                   89, Side led 18
+//  92, Side led 08
+
+void rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+
+    RGB_MATRIX_INDICATOR_SET_COLOR(68,  96, 0, 255); // left side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(71, 140, 0, 246); // left side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(74, 171, 0, 236); // left side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(77, 184, 0, 232); // left side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(81, 207, 0, 223); // left side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(84, 216, 0, 218); // left side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(88, 234, 0, 210); // left side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(92, 248, 0, 202); // left side LEDs
+
+    RGB_MATRIX_INDICATOR_SET_COLOR(69,  96, 0, 255); // right side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(72, 140, 0, 246); // right side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(75, 171, 0, 236); // right side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(78, 184, 0, 232); // right side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(82, 207, 0, 223); // right side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(85, 216, 0, 218); // right side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(89, 234, 0, 210); // right side LEDs
+    RGB_MATRIX_INDICATOR_SET_COLOR(93, 248, 0, 202); // right side LEDs
+
+    if (IS_HOST_LED_ON(USB_LED_CAPS_LOCK)) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(68, 255, 255, 255); // left side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(71, 255, 255, 255); // left side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(74, 255, 255, 255); // left side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(77, 255, 255, 255); // left side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(81, 255, 255, 255); // left side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(84, 255, 255, 255); // left side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(88, 255, 255, 255); // left side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(92, 255, 255, 255); // left side LEDs
+
+        RGB_MATRIX_INDICATOR_SET_COLOR(69, 255, 255, 255); // right side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(72, 255, 255, 255); // right side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(75, 255, 255, 255); // right side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(78, 255, 255, 255); // right side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(82, 255, 255, 255); // right side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(85, 255, 255, 255); // right side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(89, 255, 255, 255); // right side LEDs
+        RGB_MATRIX_INDICATOR_SET_COLOR(93, 255, 255, 255); // right side LEDs
+    }
+}
+
 // clang-format on
 
 #ifdef ENCODER_ENABLE
